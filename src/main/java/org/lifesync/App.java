a5db7ab48cb6ab61921d2d40438349a46acf5981
Austen Henry <austen.henry@gmail.com>
2020-05-08 08:39:36 -0500

    Setup testing.

---
+++ src/main/java/org/lifesync/App.java
@@ -0,0 +1,11 @@
+package org.lifesync;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Austen!!!");
+  }
+
+  public static boolean isTrue() {
+    return true;
+  }
+}
---
+++ src/test/java/org/lifesync/AppTest.java
@@ -0,0 +1,11 @@
+package org.lifesync;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+  @Test
+  void testApp_Success() {
+    assertTrue(App.isTrue());
+  }
+}
